02eca054f6d001088027e41aa82c49bfe59f37e7
Angelina <atvorngsam@gmail.com>
2022-03-18 22:18:09 -0500

    manager test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,11 +1,11 @@
 const Employee = require('../lib/Employee');
 
 test('create an Employee object', () => {
-    const employee = new Employee('Todd', 0, 'todd@mail.com');
+    const employee = new Employee('Ray', 0, 'ray@mail.com');
 
-    expect(employee.name).toBe('Todd');
+    expect(employee.name).toBe('Ray');
     expect(employee.id).toBe(0);
-    expect(employee.email).toBe('todd@mail.com');
-    expect(employee.getEmail()).toBe('mailto:todd@mail.com');
+    expect(employee.email).toBe('ray@mail.com');
+    expect(employee.getEmail()).toBe('mailto:ray@mail.com');
     expect(employee.getRole()).toBe('Employee');
 });
---
+++ __tests__/Manager.test.js
@@ -0,0 +1,12 @@
+const Manager = require('../lib/Manager');
+
+test('create an Manager object', () => {
+    const manager = new Manager('Ray', 0, 'ray@mail.com');
+
+    expect(manager.name).toBe('Ray');
+    expect(manager.id).toBe(0);
+    expect(manager.email).toBe('ray@mail.com');
+    expect(manager.officeNumber).toBe('(816)777-7729');
+    expect(manager.getEmail()).toBe('mailto:ray@mail.com');
+    expect(manager.getRole()).toBe('Manager');
+});
